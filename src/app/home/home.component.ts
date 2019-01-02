5487532d9903f3721d358820b18f36e4dfdab0b9
Egojose <josemicrobiologo@gmail.com>
2019-01-02 07:48:56 -0500

    se agregan usurarios y 'variable de instancia' friends

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,16 +9,54 @@ import { User } from '../interfaces/user';
 })
 export class HomeComponent implements OnInit {
 
+friends : User[];
+
   constructor() { 
-    let myUser: User =  {
+    let myUser1: User =  {
       nick: 'Jose',
       subnick: 'Hola',
       mail: 'jose@jose.com',
       edad: 35,
       friend: true,
       uid: 1
-    }
-    console.log(myUser);
+    };
+
+    let myUser2: User =  {
+      nick: 'Juan',
+      subnick: 'hello',
+      mail: 'juan@juan.com',
+      edad: 30,
+      friend: true,
+      uid: 2
+    };
+
+    let myUser3: User =  {
+      nick: 'Pedro',
+      subnick: 'hi',
+      mail: 'Pedro@pedro.com',
+      edad: 25,
+      friend: true,
+      uid: 3
+    };
+
+    let myUser4: User =  {
+      nick: 'Ana',
+      subnick: 'anita',
+      mail: 'ana@ana.com',
+      edad: 30,
+      friend: true,
+      uid: 4
+    };
+
+    let myUser5: User =  {
+      nick: 'Luis',
+      subnick: 'lucho',
+      mail: 'luis@luis.com',
+      edad: 38,
+      friend: true,
+      uid: 5
+    };
+    this.friends = [myUser1, myUser2, myUser3, myUser4, myUser5]
   }
 
   ngOnInit() {
